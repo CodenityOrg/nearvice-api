37369642b77d5a21016b066e165a7c4f2e3e15e3
Florencia Peretti <florenperetti@gmail.com>
2020-03-07 19:58:35 -0300

    install and configure helmet

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
+app.use(
+  helmet({
+    dnsPrefetchControl: { allow: true },
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"]
+      }
+    }
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
