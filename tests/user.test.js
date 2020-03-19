2f5a6aafb5cf577a3bb1ac2dc6bb4cac38d3979d
Florencia Peretti <florenperetti@gmail.com>
2020-03-19 13:13:36 -0300

    - add user mongoose model - add jest for basic user test

---
+++ tests/user.test.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const User = require('../models/user')
+
+// TODO configure to test apis
+
+const initialUsers = [
+  {
+    name: 'Florencia',
+    lastname: 'Peretti',
+    email: 'florenperetti@gmail.com',
+    password: '123145435',
+    city: 'Córdoba',
+    country: 'Argentina',
+    phone: null,
+  }
+];
+
+test('user is created', () => {
+  const firstUser = initialUsers[0];
+  const userObject = new User(firstUser);
+  expect(userObject.name).toBe(firstUser.name);
+});
