1020683168ad44404b11537122e40242c5a19363
Florencia Peretti <florenperetti@gmail.com>
2020-03-24 17:03:51 -0300

    remove password lines

--- models/specialist.js
+++ models/specialist.js
@@ -21,8 +21,6 @@ specialistSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
-    // the password should not be revealed
-    delete returnedObject.password;
   }
 })
 
