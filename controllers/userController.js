76c35f0dbb19d7015cd0b2ce82c19ea2ea9b5f7f
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-24 16:19:46 -0500

    add login response

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,26 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const errors = require('../common/errors');
 
 const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password, keepMe } = req.body;
     const user = await User.findOne({ email });
     if (!user) throw errors.notfound('User');
     if (!bcrypt.compareSync(password, user.password)) throw errors.unauthorized();
-    return res.json(user);
+    const expiresIn = keepMe ? '6h' : '1h';
+    const token = jwt.sign({ id: user._id }, req.app.get('secretKey'), { expiresIn });
+    return res.json({
+      userId: user._id,
+      token: token,
+      expiresIn: expiresIn,
+    });
   } catch (error) {
-    return Promise.reject(error);
+    return res.status(500).json({
+      status: 'error',
+      message: 'An error occurred trying to process your request',
+    });
   }
 };
 
