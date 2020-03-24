cd103ef84ad8a5adb8717ec776431f89d5a496c2
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-24 15:27:04 -0500

    add user pre save function

--- models/user.js
+++ models/user.js
@@ -1,52 +1,63 @@
-const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
+/* eslint-disable no-param-reassign */
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+const bcrypt = require('bcrypt');
+const uniqueValidator = require('mongoose-unique-validator');
 require('mongoose-type-email');
 
-const userSchema = new mongoose.Schema({
+const saltRounds = 10;
+
+const userSchema = new Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2
+    minlength: 2,
   },
   lastname: {
     type: String,
     required: true,
-    minlength: 2
+    minlength: 2,
   },
   email: {
     type: mongoose.SchemaTypes.Email,
     unique: true,
-    required: true
+    required: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   city: {
     type: String,
-    required: true
+    required: true,
   },
   country: {
     type: String,
-    required: true
+    required: true,
   },
-  phone: String
+  phone: String,
 }, {
-  timestamps: true
-})
+  timestamps: true,
+});
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
     // the password should not be revealed
-    delete returnedObject.password
-  }
-})
+    delete returnedObject.password;
+  },
+});
 
-userSchema.plugin(uniqueValidator)
+userSchema.plugin(uniqueValidator);
 
-const User = mongoose.model('User', userSchema)
+// hash user password before saving into database
+userSchema.pre('save', function (next) {
+  this.password = bcrypt.hashSync(this.password, saltRounds);
+  next();
+});
 
-module.exports = User
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
