ce919bfc91a7d86fcac5d7fc3455ac6fe80e53d7
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-30 16:21:20 -0500

    add buildError method

--- common/index.js
+++ common/index.js
@@ -2,9 +2,35 @@ const jwt = require('jsonwebtoken');
 
 const errors = require('./errors');
 
+const buildError = (res, error) => {
+  const errorData = {
+    statusCode: 500,
+    status: 'error',
+    message: '',
+  };
+  if (error && error.name === 'ValidationError') {
+    errorData.statusCode = 400;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const field in error.errors) {
+      if (error.errors[field]) {
+        errorData.message += `${error.errors[field].message} `;
+      }
+    }
+  } else {
+    errorData.message = 'An error occurred trying to process your request';
+    console.log(error);
+  }
+
+  return res.status(errorData.statusCode).json({
+    status: errorData.status,
+    message: errorData.message.trim(),
+  });
+};
+
 const generateToken = (data, expiresIn, app) => jwt.sign(data, app.get('secretKey'), { expiresIn });
 
 module.exports = {
-  ...errors,
+  errors,
+  buildError,
   generateToken,
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-const { errors, generateToken } = require('../common');
+const { errors, generateToken, buildError } = require('../common');
 
 const login = async (req, res) => {
   try {
@@ -8,19 +8,15 @@ const login = async (req, res) => {
     const user = await User.findOne({ email });
     if (!user) throw errors.notfound('User');
     if (!bcrypt.compareSync(password, user.password)) throw errors.unauthorized();
-    const expiresIn = keepMe ? '1d' : '7d';
-    const token = generateToken({ id: user._id }, expiresIn);
+    const expiresIn = keepMe ? 7 * 24 * 60 * 60 : 1 * 24 * 60 * 60;
+    const token = generateToken({ id: user._id }, expiresIn, req.app);
     return res.json({
       userId: user._id,
       token: token,
       expiresIn: expiresIn,
     });
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      status: 'error',
-      message: 'An error occurred trying to process your request',
-    });
+    return buildError(res, error);
   }
 };
 
@@ -30,15 +26,11 @@ const register = async (req, res) => {
     if (!Object.keys(data).length) throw errors.badRequest();
     const user = await User.create(data);
     const userJson = user.toJSON();
-    const expiresIn = '1d';
+    const expiresIn = 1 * 24 * 60 * 60;
     userJson.token = generateToken({ id: user._id }, expiresIn, req.app);
     return res.json(userJson);
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      status: 'error',
-      message: 'An error occurred trying to process your request',
-    });
+    return buildError(res, error);
   }
 };
 
