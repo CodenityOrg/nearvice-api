2414f7f1196a7a40b8061f71f7e8a4b4b51a4f54
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-30 16:24:56 -0500

    add reset method

--- loaders/index.js
+++ loaders/index.js
@@ -1,10 +1,16 @@
 const { connect, drop } = require('./database');
 
 const init = () => {
+  const loaders = Promise.all([connect()]);
+  return loaders;
+};
+
+const reset = () => {
   const loaders = Promise.all([drop().then(() => connect())]);
   return loaders;
 };
 
 module.exports = {
   init,
+  reset,
 };
