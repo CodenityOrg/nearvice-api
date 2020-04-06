d442f547cf9d0f2216cf00178e405ad20a0f6458
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-06 11:14:10 -0500

    add unit tests for user model

--- tests/user.test.js
+++ tests/user.test.js
@@ -7,6 +7,8 @@ beforeAll(async () => connect());
 
 afterAll(async () => drop());
 
+beforeEach(async () => User.deleteMany({}));
+
 const initialUsers = [
   {
     name: 'Florencia',
@@ -30,6 +32,23 @@ describe('User', () => {
     expect(savedUser).toHaveProperty('password');
     expect(savedUser).toHaveProperty('city', initialUsers[0].city);
     expect(savedUser).toHaveProperty('country', initialUsers[0].country);
+  });
+
+  test('hash password', async () => {
+    const validUser = new User(initialUsers[0]);
+    const savedUser = await validUser.save();
+    expect(savedUser).toHaveProperty('password');
+    expect(savedUser.password).not.toEqual(initialUsers[0].password);
+    expect(savedUser.password).toHaveLength(60);
+  });
+
+  test('use toJSON user method', async () => {
+    const validUser = new User(initialUsers[0]);
+    const savedUser = await validUser.save();
+    const userJson = savedUser.toJSON();
+    expect(userJson).not.toHaveProperty('_id');
+    expect(userJson).not.toHaveProperty('__v');
+    expect(userJson).toHaveProperty('id');
   });
 
   test('create user without required field should failed', async () => {
