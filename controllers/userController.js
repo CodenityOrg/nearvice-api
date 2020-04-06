382bef340726a4844f964f08bc6e8871e046d01b
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-06 16:36:09 -0500

    add DAY_IN_SECONDS constant

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,15 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const { errors, generateToken, buildError } = require('../common');
 
+const DAY_IN_SECONDS = 24 * 60 * 60;
+
 const login = async (req, res) => {
   try {
     const { email, password, keepMe } = req.body;
     const user = await User.findOne({ email });
     if (!user) throw errors.notfound('User');
     if (!bcrypt.compareSync(password, user.password)) throw errors.unauthorized();
-    const expiresIn = keepMe ? 7 * 24 * 60 * 60 : 1 * 24 * 60 * 60;
+    const expiresIn = keepMe ? 7 * DAY_IN_SECONDS : DAY_IN_SECONDS;
     const token = generateToken({ id: user._id }, expiresIn, req.app);
     return res.json({
       userId: user._id,
@@ -26,7 +28,7 @@ const register = async (req, res) => {
     if (!Object.keys(data).length) throw errors.badRequest();
     const user = await User.create(data);
     const userJson = user.toJSON();
-    const expiresIn = 1 * 24 * 60 * 60;
+    const expiresIn = DAY_IN_SECONDS;
     userJson.token = generateToken({ id: user._id }, expiresIn, req.app);
     return res.json(userJson);
   } catch (error) {
